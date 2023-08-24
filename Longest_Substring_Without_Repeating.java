579e63c1694123e368c8a3de2cf7695de3587219
Arpit Gupta <arpitgupta00090@gmail.com>
2023-08-24 23:57:49 +0530

    Add files via upload

--- Longest_Substring_Without_Repeating.java
+++ Longest_Substring_Without_Repeating.java
@@ -1,16 +1,53 @@
+
+// class Solution {
+//         public int lengthOfLongestSubstring(String s) {
+//                 Map<Character, Integer> map = new HashMap<>();
+//                 int i = 0;
+//                 int j = 0;
+//                 int max = 0;
+//                 while (j < s.length()) {
+//                         map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
+//                         if (map.size() == j - i + 1) {
+//                                 max = Math.max(max, j - i + 1);
+//                                 j++;
+//                         } else if (map.size() < j - i + 1) {
+//                                 while (map.size() < j - i + 1) {
+//                                         map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
+//                                         if (map.get(s.charAt(i)) == 0)
+//                                                 map.remove(s.charAt(i));
+//                                         i++;
+//                                 }
+//                                 j++;
+//                         }
+//                 }
+//                 return max;
+//         }
+// }
 import java.util.*;
 
 public class Longest_Substring_Without_Repeating {
         public static void main(String[] args) {
-                String s = "abcabcbb";
-                // String arr[]=s;
-                String s1 = new String();
-                for (int i = 0; i < s.length(); i++) {
-                        if (s.charAt(i) == s1.charAt(i)) {
-                                // s1 += s;
-                                System.out.println(s1.charAt(i));
+                // Your code here
+                Set<Character> seen = new HashSet<>();
+                int left = 0;
+                int right = 0;
+                int max = 0;
+                while (right < s.length) {
+                        char c = s.charAt(right);
+                        if (seen.add(c)) {
+                                max = Math.max(max, right + left - 1);
+                                right++;
                         }
-                        // s1 += s;
+
+                        else {
+                                while (s.charAt(left) = c) {
+                                        seen.remove(s.charAt(left));
+                                        left++;
+                                }
+                                seen.remove(c);
+                                left++;
+                        }
                 }
+                returm max;
         }
 }

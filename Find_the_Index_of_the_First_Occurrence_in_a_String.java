2db7804f4ddb31f4e34e57a1fddd25787323491d
Arpit Gupta <arpitgupta00090@gmail.com>
2023-08-29 23:19:54 +0530

    Add files via upload

---
+++ Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Find_the_Index_of_the_First_Occurrence_in_a_String {
+        public static void main(String[] args) {
+                String str1 = "sdbutsad";
+                String str2 = "sad";
+                System.out.println(str1.indexOf(str2));
+        }
+}
